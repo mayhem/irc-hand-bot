a5313a8a240092e17a79b92649dcdcbb4271e31e
Robert Kaye <rob@musicbrainz.org>
2023-09-04 22:17:14 +0200

    Software for the hand-bot is working!

--- hand.py
+++ hand.py
@@ -4,6 +4,8 @@ import irc.bot
 import irc.strings
 from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 
+from led_driver import LEDDriver
+
 NICKNAME = "hand-bot"
 
 class HandBot(irc.bot.SingleServerIRCBot):
@@ -14,6 +16,10 @@ class HandBot(irc.bot.SingleServerIRCBot):
         self.raised_nick = None
         self.user_queue = []
 
+        self.led = LEDDriver()
+        self.led.start()
+        self.led.set_pattern("idle")
+
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
 
@@ -22,57 +28,83 @@ class HandBot(irc.bot.SingleServerIRCBot):
 
     def on_pubmsg(self, c, e):
         user = e.source.nick
-        try:
-            nick, msg = e.arguments[0].split(":")
-            if nick.lower() == NICKNAME:
-                self.do_command(user, msg)
-        except ValueError:
-            pass
-            
-        return
+        msg = e.arguments[0]
+        self.do_command(user, msg)
+
+    def set_state(self, state):
+
+        if state == "raised":
+            if self.is_raised:
+                return
+
+            self.is_raised = True
+            self.led.set_pattern(state)
+            return
+
+        if state == "idle":
+            if not self.is_raised:
+                return
+
+            self.is_raised = False
+            self.led.set_pattern(state)
+            return
+
+        if state == "rainbow":
+            if self.is_raised:
+                return
+
+            self.led.set_pattern(state)
+            return
 
     def do_command(self, user, msg):
         c = self.connection
 
         msg = msg.strip()
-        if msg == "raise":
-            if user not in self.user_queue:
+        if msg == "!raise":
+            if user in self.user_queue:
+                c.privmsg(self.channel, f"{user}: you already raised your hand. wait your turn!")
+                return
+            else:
+                self.set_state("raised")
                 self.user_queue.append(user)
                 position = self.user_queue.index(user)
                 c.privmsg(self.channel, f"{user}: you raised your hand and you're in position {position}")
-            else:
-                c.privmsg(self.channel, f"{user}: you already raised your hand. wait your turn!")
                 return
 
-        elif msg == "lower":
+        elif msg == "!lower":
             if user not in self.user_queue:
                 c.privmsg(self.channel, f"{user}: your hand wasn't raised, silly!")
             else:
                 self.user_queue.remove(user)
                 c.privmsg(self.channel, f"{user}: you lowered your hand")
-        elif msg == "queue":
+
+            if len(self.user_queue) == 0:
+                self.set_state("idle")
+
+        elif msg == "!queue":
 
             if len(self.user_queue) == 0:
                 c.privmsg(self.channel, f"the queue is empty")
             else:
                 c.privmsg(self.channel, f"the current queue: " + ", ".join(self.user_queue))
-        elif msg == "die":
+        elif msg == "!die":
             c.privmsg(self.channel, f"Goodbye cruel world!")
             self.die()
             sys.exit(-1)
-        elif msg == "love":
+        elif msg == "!love":
             c.privmsg(self.channel, f"Awwww, I love you too!")
-        elif msg == "rick":
+        elif msg == "!rainbow":
+            self.set_state("rainbow")
+            c.privmsg(self.channel, f"Is it party time?")
+        elif msg == "!rick":
             c.privmsg(self.channel, f"https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-        elif msg == "help":
+        elif msg == "!help":
             c.privmsg(self.channel, f"I respond to the following commands:")
             c.privmsg(self.channel, f"  raise - raise your hand")
             c.privmsg(self.channel, f"  lower - lower your hand")
             c.privmsg(self.channel, f"  queue - show the current queue of people waiting")
             c.privmsg(self.channel, f"  love - show some love")
             c.privmsg(self.channel, f"  rick - how could we not?")
-        else:
-            c.privmsg(self.channel, f"{user}: i didn't understand your command: '{msg}'")
 
 def main():
     try:
--- led_driver.py
+++ led_driver.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-import time
+from colorsys import hsv_to_rgb
+from threading import Thread
+from time import sleep
 from rpi_ws281x import PixelStrip, Color
 
 LED_COUNT = 4        # Number of LED pixels.
@@ -12,43 +14,95 @@ LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 
-class LEDDriver:
+class LEDDriver(Thread):
 
     def __init__(self):
+        Thread.__init__(self)
         self.strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         self.strip.begin()
         self.next_update = None
+        self.rainbow_hue_index = 0.0
+        self.raised_hue_index = 0.0
+        self.raised_increment = None
 
-    def update(self):
-        pass
+        self.startup()
+
+    def startup(self):
+        for i in range(3):
+            self.strip.setPixelColor(0, Color(  0, 180, 5))
+            self.strip.setPixelColor(1, Color(  0, 180, 5))
+            self.strip.setPixelColor(2, Color(255, 50, 0))
+            self.strip.setPixelColor(3, Color(255, 50, 0))
+            self.strip.show()
+            sleep(.100)
+
+            self.strip.setPixelColor(0, Color(255, 50, 0))
+            self.strip.setPixelColor(1, Color(255, 50, 0))
+            self.strip.setPixelColor(2, Color(  0, 180, 5))
+            self.strip.setPixelColor(3, Color(  0, 180, 5))
+            self.strip.show()
+            sleep(.100)
 
     def idle(self, wait_ms=20, iterations=1):
-        self.strip.setPixelColor(0, Color(  0, 180, 5))
-        self.strip.setPixelColor(1, Color(  0, 180, 5))
-        self.strip.setPixelColor(2, Color(255, 50, 0))
-        self.strip.setPixelColor(3, Color(255, 50, 0))
-        self.strip.show()
+        self.strip.setPixelColor(0, Color(  0, 32, 0))
+        self.strip.setPixelColor(1, Color(  0, 32, 0))
+        self.strip.setPixelColor(2, Color(64, 18, 0))
+        self.strip.setPixelColor(3, Color(64, 18, 0))
+        return .1 
+
+    def raised(self):
+        hue_begin = 0.0
+        hue_end = .03
+        steps = 50
+
+        if self.raised_increment is None:
+            self.raised_increment = (hue_end - hue_begin) / steps
+
+        self.raised_hue_index += self.raised_increment
+        if self.raised_hue_index > hue_end:
+            self.raised_hue_index = hue_end - self.raised_increment
+            self.raised_increment = -self.raised_increment
+        elif self.raised_hue_index < hue_begin:
+            self.raised_hue_index = hue_begin - self.raised_increment
+            self.raised_increment = -self.raised_increment
+
+        r, g, b = hsv_to_rgb(self.raised_hue_index, 1.0, 1.0)
+        for i in range(4):
+            self.strip.setPixelColor(i, Color(int(r * 255), int(g * 255), int(b * 255)))
+
+        return .015 
 
     def rainbow(self):
+        r, g, b = hsv_to_rgb(self.rainbow_hue_index, 1.0, 1.0)
+        for i in range(4):
+            self.strip.setPixelColor(i, Color(int(r * 255), int(g * 255), int(b * 255)))
+        self.rainbow_hue_index += .001
 
-        def wheel(pos):
-            """Generate rainbow colors across 0-255 positions."""
-            if pos < 85:
-                return Color(pos * 3, 255 - pos * 3, 0)
-            elif pos < 170:
-                pos -= 85
-                return Color(255 - pos * 3, 0, pos * 3)
-            else:
-                pos -= 170
-                return Color(0, pos * 3, 255 - pos * 3)
+        return .01
 
-        for j in range(256 * iterations):
-            for i in range(self.strip.numPixels()):
-                self.strip.setPixelColor(i, wheel((i + j) & 255))
+    def set_pattern(self, pattern):
+        if pattern not in ("idle", "raised", "rainbow"):
+            return
+        self.pattern = pattern
+
+    def run(self):
+
+        self.pattern = "idle"
+        while True:
+            if self.pattern == "idle":
+                delay = self.idle()
+            elif self.pattern == "raised":
+                delay = self.raised()
+            elif self.pattern == "rainbow":
+                delay = self.rainbow()
+
             self.strip.show()
-            time.sleep(wait_ms / 1000.0)
+            sleep(delay)
+
 
 if __name__ == '__main__':
     d = LEDDriver()
-#    d.rainbow(iterations=9999999)
-    d.idle()
+    d.start()
+    d.set_pattern("raised")
+    while True:
+        sleep(1)
